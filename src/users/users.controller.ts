fa07e230cb4336960313e20a844a1e92db85bdd6
Zinger Marat <zingermarat@gmail.com>
2025-12-17 19:32:46 +0200

    feat: add CRUD operations to UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,12 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   NotFoundException,
   Param,
   Post,
+  Put,
   Query,
 } from '@nestjs/common';
 
@@ -25,5 +28,26 @@ export class UsersController {
   getUserById(@Param('id') id: string) {
     if (!Number(id)) throw new NotFoundException('not valid id');
     return `User with id: ${id}`;
+  }
+
+  @Post()
+  createUser(@Body() body: any) {
+    if (!body) throw new NotFoundException('body required');
+
+    return {
+      message: 'User created',
+      code: 201,
+      data: body,
+    };
+  }
+
+  @Put(':id')
+  updateUser() {
+    return 'Update user';
+  }
+
+  @Delete(':id')
+  deleteUser() {
+    return 'Delete user';
   }
 }
